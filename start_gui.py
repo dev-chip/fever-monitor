d248ea8359c32091c8e55d074588e77cf644e967
Chip <chip.ck.main@gmail.com>
2021-03-24 07:21:06 +0000

    Sprint 4: Added window icon.

--- start_gui.py
+++ start_gui.py
@@ -10,17 +10,29 @@ __maintainer__ = "James Cook"
 __email__ = "contact@cookjames.uk"
 __status__ = "Prototype"
 
-
+# external module imports
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 import sys
+import os
+import ctypes
 
+# project imports
 from qtgui.controller import Controller
 from qtgui.cfg import get_configs
+from qtgui.logger import (set_logger_level,
+                          init_console_logger)
 
-from qtgui.logger import set_logger_level, init_console_logger
 logger = init_console_logger(name="gui")
-
 version_id = 1.0
+
+# global path variable definitions
+THIS_PATH = os.path.abspath(os.path.dirname(__file__))
+IMG_PATH = os.path.abspath(os.path.join(THIS_PATH, "qtgui", "img"))
+
+# set applications properties
+app_id = 'fever_monitor.' + str(version_id)
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
 
 
 def print_pretty_name():
@@ -40,6 +52,7 @@ if __name__ == '__main__':
     # start GUI
     logger.info("Starting GUI...")
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(os.path.join(IMG_PATH, 'icon.png')))
     controller = Controller()
     controller.show_main()
     app.exec_()
