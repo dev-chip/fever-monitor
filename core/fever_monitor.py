c6ce3dba55d5c610a5d3295c18984adb00c92108
Chip <chip.ck.main@gmail.com>
2021-04-11 19:18:48 +0100

    Added profiling script to tools.

--- core/fever_monitor.py
+++ core/fever_monitor.py
@@ -319,33 +319,3 @@ class FeverMonitor:
 
         return pil_image, face_objects
 
-
-# --------------------- #
-#     Profile Code      #
-# --------------------- #
-if __name__ == "__main__":
-    # import profiling modules
-    import pstats
-    import cProfile
-
-    # instantiate and enable the profiler
-    pr = cProfile.Profile()
-    pr.enable()
-
-    # run inference
-    fever_monitor = FeverMonitor(
-        temp_threshold=38.0,
-        temp_unit="Celsius",
-        colormap_index=5,
-        yolo_model="Standard",
-        confidence_threshold=0.5,
-        use_gpu=False)
-    for i in range(100):
-        fever_monitor.run()
-
-    # disable the profiler
-    pr.disable()
-
-    # output the results statistics
-    stats = pstats.Stats(pr).sort_stats('cumtime')
-    stats.print_stats()
